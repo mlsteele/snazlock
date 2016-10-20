39675c7274b05a56212e865b5af16fd367547ac0
Miles Steele <miles@milessteele.com>
2016-10-20 18:45:06 -0400

    auth works

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 extern crate pam_auth;
-pub fn main() {
-        let service = "<yourapp>";
-        let user = "<user>";
-        let password = "<pass>";
+extern crate rpassword;
+extern crate users;
 
-        let mut auth = pam_auth::Authenticator::new(service).unwrap();
-        auth.set_credentials(user, password);
-        if auth.authenticate().is_ok() && auth.open_session().is_ok() {
-            println!("Successfully opened a session!");
-        }
-        else {
-            println!("Authentication failed =/");
-        }
+
+const PAM_APP_NAME: &'static str = "snazlock";
+
+pub fn main() {
+    let username = users::get_user_by_uid(users::get_current_uid()).unwrap().name().to_owned();
+    println!("Hello, {}!", username);
+
+    println!("password me up >");
+    let password = rpassword::read_password().unwrap();
+    println!("got it.");
+
+    let mut auth = pam_auth::Authenticator::new(PAM_APP_NAME).unwrap();
+    auth.set_credentials(&username, &password);
+    if auth.authenticate().is_ok() && auth.open_session().is_ok() {
+        println!("Successfully opened a session!");
+    }
+    else {
+        println!("Authentication failed =/");
+    }
 }
