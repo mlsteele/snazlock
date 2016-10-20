5bbc29e1a3b71fe199ce7c04f7241d9296086148
Miles Steele <miles@milessteele.com>
2016-10-20 18:27:37 -0400

    get pam

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+extern crate pam_auth;
+pub fn main() {
+        let service = "<yourapp>";
+        let user = "<user>";
+        let password = "<pass>";
+
+        let mut auth = pam_auth::Authenticator::new(service).unwrap();
+        auth.set_credentials(user, password);
+        if auth.authenticate().is_ok() && auth.open_session().is_ok() {
+            println!("Successfully opened a session!");
+        }
+        else {
+            println!("Authentication failed =/");
+        }
 }
